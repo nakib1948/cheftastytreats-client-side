b0ff1429cf290ea2060869f8764faabf4557878b
nakib1948 <hayathossain.jtp@gmail.com>
2023-06-25 10:26:34 +0600

    Nabar and footer design complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from './Layout/Main.jsx';
+import Home from './Pages/Home.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      }
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="mx-auto max-w-7xl">
+    <React.StrictMode>
+    
+        <RouterProvider router={router} />
+      
+    </React.StrictMode>
+  </div>
+);
